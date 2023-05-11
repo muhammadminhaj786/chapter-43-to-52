d34baa066b2d5f677d92b7bbe964caa03a1898fe
M Minhaj <mminhajwahid0316@gmail.comminhaj0316>
2023-05-11 08:12:48 -0700

    half complete

---
+++ app.js
@@ -0,0 +1,33 @@
+// Chapter 42 to 48
+// Question No 1 and 2
+// function func(){
+//     alert("Thanks for purchasing a phone from us")
+// }
+
+// Question No 3  *********
+// function del(){
+//     document.getElementById("myTable").deleteRow(0);
+    
+// }
+
+// Question No 4
+
+// function changeImg(flag){
+//     var img_store = document.getElementById("Img")
+//     if (flag=='in'){
+//         img_store.src = "./images/car2.jpg"
+//     }else{
+//         img_store.src = "./images/car1.jpg"
+//     }
+// }
+
+// Question No 5
+// var input = document.getElementById("Inp")
+// input.value  = 0
+
+// function addCount(){
+//     input.value += 1
+// }
+// function delCount(){
+//     input.value -= 1
+// }
